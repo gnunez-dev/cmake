50aa2c585b7d2b2146cd53b3558a62c3c28efe8e
gnunez-dev <gnunez.dev@gmail.com>
2023-07-06 21:07:14 -0500

    added FetchContent

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 
+#include <cxxopts.hpp>
 #include <nlohmann/json.hpp>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
+
 #include "my_lib.h"
 #include "config.hpp"
 
 int main(){
 
     std::cout << project_name << std::endl;
-    std::cout << project_VERSION << std::endl;
+    std::cout << project_VERSION << std::endl << std::endl;
     printHello();
 
     std::cout << "JSON Lib Version:" << std::endl
               << NLOHMANN_JSON_VERSION_MAJOR << "."
               << NLOHMANN_JSON_VERSION_MINOR << "."
-              << NLOHMANN_JSON_VERSION_PATCH << "." << std::endl;
+              << NLOHMANN_JSON_VERSION_PATCH << std::endl << std::endl;
+
+    std::cout << "FMT Lib Version:" << std::endl
+              << FMT_VERSION << std::endl << std::endl;
+
+    std::cout << "CXXOPTS Lib Version:" << std::endl
+              << CXXOPTS__VERSION_MAJOR << "."
+              << CXXOPTS__VERSION_MINOR << "."
+              << CXXOPTS__VERSION_PATCH << std::endl << std::endl;
+
+    std::cout << "SPDLOG Lib Version:" << std::endl
+              << SPDLOG_VER_MAJOR << "."
+              << SPDLOG_VER_MINOR << "."
+              << SPDLOG_VER_PATCH << std::endl << std::endl;
+
     return 0;
 }
--- src/my_lib/my_lib.cpp
+++ src/my_lib/my_lib.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
 #include "my_lib.h"
 
 void printHello(){
     std::cout << "Hello World\n";
+
+    std::cout << "JSON Lib Version:" << std::endl
+              << NLOHMANN_JSON_VERSION_MAJOR << "."
+              << NLOHMANN_JSON_VERSION_MINOR << "."
+              << NLOHMANN_JSON_VERSION_PATCH << "." << std::endl;
 }
